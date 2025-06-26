b386d8a480e028e5a675e6eeb35b0d9f518093c3
LDH <leedoh0384@gmail.com>
2025-06-26 15:16:29 +0900

    계산기 3일차

--- addition.js
+++ addition.js
@@ -1,94 +1,103 @@
-const exprInput = document.getElementById("calc-form")
+const exprForm = document.getElementById("calc-form")
     .addEventListener("submit", function (e) {
         e.preventDefault(); // html로 못가게 막음
 
-        // input 요소 가져오기
         const exprInput = document.getElementById("expression");
-        // 값 읽기
         const userText = exprInput.value.trim();
 
         if (userText === "exit") {
             alert("종료");
+        } else if (userText === ""){
+            alert("값이 없습니다.");
         } else {
-            parseExpr(userText);
+            main(userText);
         }
     });
 
-// 문자, 숫자 분리해주기
-function parseExpr(userText) {
-    const sliceText = userText.match(/(\d+|\+|\-|\*|\/|\(|\))/g);
-    // console.log(sliceText);
-    tokenize(sliceText);
+function main(userText) {
+    const sliceText = parseExpr(userText);
+    const slicenumText = tokenize(sliceText);
+    const postfix = calcPriority(slicenumText);
+    const [result] = basic_ari(postfix);
+    printMessage(result);
 }
 
-// 출력기능 함수
-// function printMessage() {
-
-// }
-
-// 계산하기
-function basic_ari(ans) {
-    console.log(ans);
+// 문자열 파싱
+function parseExpr(tokens) {
+    const sliceText = tokens.match(/\d+|[+\-*/()]/g);
+    return sliceText;
 }
 
 // 숫자인 문자를 숫자로 바꾸기
 function tokenize(sliceText) {
-    const slicenumText = sliceText.map(a => isNaN(a) ? a : Number(a));
-    // console.log(slicenumText);
-    calcPriority(slicenumText);
+    return sliceText.map(a => isNaN(a) ? a : Number(a));
 }
 
-// 우선순위 계산하기
+// 우선순위 계산하기 (후위표기식)
 function calcPriority(sliceText) {
-    let ans = "";
+    const output = [];
     const stack = [];
-    for (let char of sliceText) {
-        if (typeof char === 'number') {
-            ans += char;
+    const priority = {
+        '+': 1,
+        '-': 1,
+        '*': 2,
+        '/': 2,
+    };
+
+    for (let token of sliceText) {
+        if (typeof token === 'number') {
+            output.push(token);
         } else {
-            switch (char) {
-                case "(":
-                    stack.push("(");
-                    break;
-                case "+":
-                    while (stack.length > 0 && stack[stack.length - 1] != "(") {
-                        ans += stack.pop();
-                    }
-                    stack.push("+");
-                    break;
-                case "-":
-                    while (stack.length > 0 && stack[stack.length - 1] != "(") {
-                        ans += stack.pop();
-                    }
-                    stack.push("-");
-                    break;
-                case "/":
-                    while (
-                        stack.length > 0 &&
-                        (stack[stack.length - 1] == "/" || stack[stack.length - 1] == "*")) {
-                        ans += stack.pop();
-                    }
-                    stack.push("/");
-                    break;
-                case "*":
-                    while (
-                        stack.length > 0 &&
-                        (stack[stack.length - 1] == "/" || stack[stack.length - 1] == "*")) {
-                        ans += stack.pop();
-                    }
-                    stack.push("*");
-                    break;
-                case ")":
-                    while (stack.length > 0 && stack[stack.length - 1] != "(") {
-                        ans += stack.pop();
-                    }
-                    stack.pop();
-                    break;
+            while (
+                stack.length > 0 &&
+                priority[stack[stack.length - 1]] >= priority[token]
+            ) {
+                output.push(stack.pop());
             }
+            stack.push(token);
         }
     }
-    while (stack.length) {
-        ans += stack.pop();
+
+    while (stack.length > 0) {
+        output.push(stack.pop());
     }
-    basic_ari(ans);
+
+    return output;
+}
+
+// 후위표기식 하나씩 분리해서 ari로 보내기
+function basic_ari(ans) {
+    const stack = [];
+
+    for (let token of ans) {
+        if (typeof token === 'number') {
+            stack.push(token)
+        } else {
+            let a = stack.pop();
+            let b = stack.pop();
+            stack.push(ari(a, b, token));
+        }
+    }
+    return stack;
+}
+
+// 계산하기
+function ari(a, b, token) {
+    switch (token) {
+        case "+":
+            return (a + b);
+        case "-":
+            return (b - a);
+        case "*":
+            return (a * b);
+        case "/":
+            const num = b / a;
+            return Number.isInteger(num) ? num : Number(num.toFixed(2));
+    }
+}
+
+// 출력하기
+function printMessage(result) {
+    const results = document.getElementById("result");
+    results.innerText = `결과: ${result}`;
 }
